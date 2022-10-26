29dfb8c20a35e88116c993bca4f483fb4281ba13
maria <filippovamarusya@gmail.com>
2022-10-26 21:21:32 +0300

    module2-task1: add screens

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
+import { FILMS } from './mocks/films';
+
+const FilmCard = {
+  Title: 'The Grand Budapest Hotel',
+  Genre: 'Drama',
+  Year: 2014,
+} as const;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -8,6 +15,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App filmPromo={{title: FilmCard.Title, genre: FilmCard.Genre, year: FilmCard.Year}} cards={FILMS}/>
   </React.StrictMode>,
 );
