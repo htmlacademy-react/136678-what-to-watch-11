27b12539c4dde1e7c5fc98cf822a77ba1928747c
maria <filippovamarusya@gmail.com>
2022-10-27 18:17:32 +0300

    module3-task1: add routes

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+
+import AddReviewScreen from '../../pages/add-review-screen/add-review-screen';
+import FilmScreen from '../../pages/film-screen/film-screen';
 import MainScreen from '../../pages/main-screen/main-screen';
+import MyListScreen from '../../pages/my-list-screen/my-list-screen';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
+import PlayerScreen from '../../pages/player-screen/player-screen';
+import PrivateRoute from '../private-route/private-route';
+import SignInScreen from '../../pages/sign-in-screen/sign-in-screen';
+
+import { AppRoute, AuthorizationStatus } from '../../const';
 import { Film } from '../../types/film';
 
 type AppScreenProps = {
@@ -11,7 +23,54 @@ type AppScreenProps = {
 };
 
 function App(props: AppScreenProps): JSX.Element {
-  return <MainScreen {...props} />;
+  return (
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path={AppRoute.Main}
+            element={<MainScreen {...props} />}
+          />
+
+          <Route
+            path={AppRoute.Film}
+            element={<FilmScreen />}
+          />
+
+          <Route
+            path={AppRoute.AddReview}
+            element={<AddReviewScreen />}
+          />
+
+          <Route
+            path={AppRoute.Player}
+            element={<PlayerScreen />}
+          />
+
+          <Route
+            path={AppRoute.SignIn}
+            element={<SignInScreen />}
+          />
+
+          <Route
+            path={AppRoute.MyList}
+            element={
+              <PrivateRoute
+                authorizationStatus={AuthorizationStatus.NoAuth}
+              >
+                <MyListScreen />
+              </PrivateRoute>
+            }
+          />
+
+          <Route
+            path={AppRoute.NotFound}
+            element={<NotFoundScreen />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
+  );
 }
 
 export default App;
--- project/src/components/small-film-card/small-film-card.tsx
+++ project/src/components/small-film-card/small-film-card.tsx
@@ -1,16 +1,15 @@
-type SmallFilmCardProps = {
-  title: string;
-  src: string;
-}
+import { Link } from 'react-router-dom';
 
-function SmallFilmCard({title, src}: SmallFilmCardProps): JSX.Element {
+import { Film } from '../../types/film';
+
+function SmallFilmCard({ id, title, src }: Film): JSX.Element {
   return (
     <article className="small-film-card catalog__films-card">
       <div className="small-film-card__image">
         <img src={src} alt="Fantastic Beasts: The Crimes of Grindelwald" width="280" height="175" />
       </div>
       <h3 className="small-film-card__title">
-        <a className="small-film-card__link" href="film-page.html">{title}</a>
+        <Link className="small-film-card__link" to={`/films/${id}`}>{title}</Link>
       </h3>
     </article>
   );
