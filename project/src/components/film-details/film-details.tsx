b5118aa8736a797c83c2aaec3a7b167ed929b2d6
maria <filippovamarusya@gmail.com>
2022-11-06 13:49:47 +0100

    module5-task2: add film tabs

---
+++ project/src/components/film-details/film-details.tsx
@@ -0,0 +1,49 @@
+import { Film } from '../../types/film';
+
+type FilmDetailsProps = {
+  film: Film;
+}
+
+function FilmDetails({ film }: FilmDetailsProps): JSX.Element {
+  const {
+    director,
+    starring,
+    genre,
+    released,
+    runTime
+  } = film;
+
+  const actorList = starring.join(', ');
+
+  return (
+    <div className="film-card__text film-card__row">
+      <div className="film-card__text-col">
+        <p className="film-card__details-item">
+          <strong className="film-card__details-name">Director</strong>
+          <span className="film-card__details-value">{director}</span>
+        </p>
+        <p className="film-card__details-item">
+          <strong className="film-card__details-name">Starring</strong>
+          <span className="film-card__details-value">{actorList}</span>
+        </p>
+      </div>
+
+      <div className="film-card__text-col">
+        <p className="film-card__details-item">
+          <strong className="film-card__details-name">Run Time</strong>
+          <span className="film-card__details-value">{`${Math.floor(runTime / 60)}h ${runTime % 60}m`}</span>
+        </p>
+        <p className="film-card__details-item">
+          <strong className="film-card__details-name">Genre</strong>
+          <span className="film-card__details-value">{genre}</span>
+        </p>
+        <p className="film-card__details-item">
+          <strong className="film-card__details-name">Released</strong>
+          <span className="film-card__details-value">{released}</span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default FilmDetails;
---
+++ project/src/components/film-overview/film-overview.tsx
@@ -0,0 +1,55 @@
+import { Film } from '../../types/film';
+import { RatingLevel } from '../../const';
+
+type FilmOverviewProps = {
+  film: Film;
+}
+
+function FilmOverview({ film }: FilmOverviewProps): JSX.Element {
+  const {
+    rating,
+    scoresCount,
+    description,
+    director,
+    starring,
+  } = film;
+
+  const actorList = starring.join(', ');
+
+  const getRatingLevel = () => {
+    if (rating < 3) {
+      return RatingLevel.Bad;
+    }
+    if (rating < 5) {
+      return RatingLevel.Normal;
+    }
+    if (rating < 8) {
+      return RatingLevel.Good;
+    }
+    if (rating < 10) {
+      return RatingLevel.VeryGood;
+    }
+    if (rating === 10) {
+      return RatingLevel.Awesome;
+    }
+  };
+
+  return (
+    <>
+      <div className="film-rating">
+        <div className="film-rating__score">{ rating }</div>
+        <p className="film-rating__meta">
+          <span className="film-rating__level">{ getRatingLevel() }</span>
+          <span className="film-rating__count">{ scoresCount } ratings</span>
+        </p>
+      </div>
+      <div className="film-card__text">
+        <p>{ description }</p>
+        <p className="film-card__director"><strong>Director: { director }</strong></p>
+        <p className="film-card__starring"><strong>Starring: { actorList }</strong></p>
+      </div>
+    </>
+  );
+}
+
+export default FilmOverview;
